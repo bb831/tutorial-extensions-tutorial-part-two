d1633cabfb1b5878a1fb1b94a0d370538a244c01
Clarke <Victor.A.Clarke@medstar.net>
2020-11-05 15:12:32 -0500

    created new files

--- js/application_doughnut_v2.js
+++ js/application_doughnut_v2.js
@@ -2,49 +2,55 @@
   
 (function () {
    $(document).ready(function () {
-      // Initialises Tableau Data Extension
-      tableau.extensions.initializeAsync().then(function () {
-      // Once we initialize we call teh drawChartJS function.
-      drawChartJS();
-   }, function () { console.log('Error while Initializing: ' + err.toString()); });
+      // Added new code here to point to the configure function.
+      tableau.extensions.initializeAsync({ 'configure':configure }).then(function () {
+         drawChartJS();
+      }, function () { console.log('Error while Initializing: ' + err.toString()); });
    });
- 
-   // This javascript function gets data from our worksheet and draws the Doughnut.
+  
    function drawChartJS() {
-      // Gets all the worksheets in a Tableau Dashboard
-      const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
-      // Finds a worksheet called worksheetData
-      var worksheet = worksheets.find(function (sheet) {
-         return sheet.name === "worksheetData";
+      const worksheets=tableau.extensions.dashboardContent.dashboard.worksheets;
+      var worksheet=worksheets.find(function (sheet) {
+         return sheet.name==="worksheetData";
       });
- 
-      // Call a function on the worksheet Object to get the Summary Data.
+       
       worksheet.getSummaryDataAsync().then(function (sumdata) {
-         // Create an empty arrays for our labels and data set.
          var labels = [];
          var data = [];
-          
-         // We get our summary data:
          var worksheetData = sumdata.data;
-         // We loop through our summary data and hardcode which columns goes into Label
-         // and which column goes into the array.
-         for (var i = 0; i < worksheetData.length; i++) {
+  
+         for (var i=0; i < worksheetData.length; i++) {
             labels.push(worksheetData[i][0].formattedValue);
             data.push(worksheetData[i][1].value);
          }
  
-         // Draw the chart as before.
          var ctx = $("#myChart");
          var myChart = new Chart(ctx, {
             type: 'doughnut',
             data: {
-               labels: labels, // This now comes from Tableau
+               labels: labels,
                datasets: [{
                   backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
-                  data: data // This now comes from Tableau
+                  data: data
                }]
             }
          });
+      });
+   }
+ 
+   // This opens the configuration window.
+   function configure() {
+   const popupUrl = `${window.location.origin}/dialog.html`;
+   let defaultPayload = "";
+   tableau.extensions.ui.displayDialogAsync(popupUrl, defaultPayload, { height:300, width:500 }).then((closePayload) => {
+   }).catch((error) => {
+      switch (error.errorCode) {
+         case tableau.ErrorCodes.DialogClosedByUser:
+            console.log("Dialog was closed by user");
+         break;
+         default:
+            console.error(error.message);
+         }
       });
    }
 })();
